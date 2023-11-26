34d29fe25202069f22cc055abd6fd6cf7aba817f
omaremalik <omarmalik2001@gmail.com>
2023-11-25 19:27:43 -0500

    changed the file read method and the  method that populates the response

--- InputReader.java
+++ InputReader.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
  * The text typed by a user is then chopped into words, and a set of words 
  * is provided.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Omar Malik.
+ * @version 2023.11.25
  */
 public class InputReader
 {
--- Responder.java
+++ Responder.java
@@ -14,8 +14,8 @@ import java.util.*;
  * in the HashMap, the corresponding response is returned. If none of the input
  * words is recognized, one of the default responses is randomly chosen.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Omar Malik.
+ * @version 2023.11.25
  */
 public class Responder
 {
@@ -34,7 +34,7 @@ public class Responder
     {
         responseMap = new HashMap<>();
         defaultResponses = new ArrayList<>();
-        fillResponseMap();
+        fillResponseMapFromFile();
         fillDefaultResponses();
         randomGenerator = new Random();
     }
@@ -65,83 +65,76 @@ public class Responder
      * Enter all the known keywords and their associated responses
      * into our response map.
      */
-    private void fillResponseMap()
-    {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+    private void fillResponseMapFromFile() {
+    Charset charset = Charset.forName("US-ASCII");
+    Path path = Paths.get(FILE_OF_DEFAULT_RESPONSES);
+    try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+        String line = reader.readLine();
+
+        while (line != null) {
+            // Split the line into key and values using a comma as a separator
+            String[] parts = line.split(",");
+            
+            // If there are at least two parts (key and response), add them to the map
+            if (parts.length >= 2) {
+                String key = parts[0].trim();
+                String response = parts[1].trim();
+                responseMap.put(key, response);
+            }
+
+            line = reader.readLine();
+        }
+    } catch (FileNotFoundException e) {
+        System.err.println("Unable to open " + FILE_OF_DEFAULT_RESPONSES);
+    } catch (IOException e) {
+        System.err.println("A problem was encountered reading " +
+                FILE_OF_DEFAULT_RESPONSES);
+    }
     }
 
     /**
      * Build up a list of default responses from which we can pick
      * if we don't know what else to say.
      */
-    private void fillDefaultResponses()
-    {
-        Charset charset = Charset.forName("US-ASCII");
-        Path path = Paths.get(FILE_OF_DEFAULT_RESPONSES);
-        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
-            String response = reader.readLine();
-            while(response != null) {
-                defaultResponses.add(response);
-                response = reader.readLine();
+    private void fillDefaultResponses() {
+    Charset charset = Charset.forName("US-ASCII");
+    Path path = Paths.get(FILE_OF_DEFAULT_RESPONSES);
+    try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+        String response = "";
+        String line = reader.readLine();
+        
+        while (line != null) {
+            // If the line is not empty, append it to the current response
+            if (!line.trim().isEmpty()) {
+                response += line + " ";
+            } else {
+                // If the line is empty, consider it as the end of the current entry
+                if (!response.trim().isEmpty()) {
+                    defaultResponses.add(response.trim());
+                    response = ""; // Reset for the next entry
+                }
             }
+            
+            line = reader.readLine();
         }
-        catch(FileNotFoundException e) {
-            System.err.println("Unable to open " + FILE_OF_DEFAULT_RESPONSES);
+
+        // Add the last entry if it is not empty
+        if (!response.trim().isEmpty()) {
+            defaultResponses.add(response.trim());
         }
-        catch(IOException e) {
-            System.err.println("A problem was encountered reading " +
-                               FILE_OF_DEFAULT_RESPONSES);
-        }
-        // Make sure we have at least one response.
-        if(defaultResponses.size() == 0) {
-            defaultResponses.add("Could you elaborate on that?");
-        }
+    } catch (FileNotFoundException e) {
+        System.err.println("Unable to open " + FILE_OF_DEFAULT_RESPONSES);
+    } catch (IOException e) {
+        System.err.println("A problem was encountered reading " +
+                FILE_OF_DEFAULT_RESPONSES);
     }
+
+    // Make sure we have at least one response.
+    if (defaultResponses.size() == 0) {
+        defaultResponses.add("Could you elaborate on that?");
+    }
+    }
+
 
     /**
      * Randomly select and return one of the default responses.
